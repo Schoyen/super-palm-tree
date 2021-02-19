fa5be7092ba4c6d780a102763ea4509e1c4cc602
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2021-02-19 14:32:06 +0100

    Implement spt-function in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,40 @@
+use ndarray::Array2;
+use numpy::{PyArray2, ToPyArray};
 use pyo3::prelude::*;
+use pyo3::{types::PyList, wrap_pyfunction};
+
+fn calc_spt(spt_i: &Vec<f64>, spt_j: &Vec<f64>) -> f64 {
+    let mut val = 0.0;
+
+    for (attr_i, attr_j) in spt_i.iter().zip(spt_j.iter()) {
+        val += attr_i * attr_j;
+    }
+
+    val
+}
+
+#[pyfunction]
+fn construct_calc_mat<'a>(
+    py: Python<'a>,
+    spt_params: &'a PyList,
+) -> &'a PyArray2<f64> {
+    let param_vec = spt_params.extract::<Vec<Vec<f64>>>().unwrap();
+    let l = param_vec.len();
+
+    let mut arr: Array2<f64> = Array2::zeros((l, l));
+
+    for i in 0..l {
+        for j in 0..l {
+            arr[[i, j]] = calc_spt(&param_vec[i], &param_vec[j]);
+        }
+    }
+
+    arr.to_pyarray(py)
+}
 
 #[pymodule]
 fn spt_lib(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(construct_calc_mat))?;
+
     Ok(())
 }
--- tests/test_spt.py
+++ tests/test_spt.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 from super_palm_tree import SuperPalmTree, construct_calc_mat
+from super_palm_tree.spt_lib import (
+    construct_calc_mat as construct_calc_mat_rust,
+)
 
 
 def test_spt():
@@ -28,3 +31,8 @@ def test_calc_mat():
     c = construct_calc_mat(spts)
 
     np.testing.assert_allclose(c, c.T)
+
+    spt_params = [[*spt.unpack()] for spt in spts]
+    c_2 = construct_calc_mat_rust(spt_params)
+
+    np.testing.assert_allclose(c, c_2)
